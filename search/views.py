eae041dd4c04527ad9f18bba04e752104a60f53b
floresjmjr <floresjmjr@gmail.com>
2021-03-13 23:39:43 -0500

    search nutrient breakdown logic

--- search/views.py
+++ search/views.py
@@ -27,13 +27,55 @@ def results(request):
 
 
 def foodItem(request, food_id):
+  # Get nutritional data from API
   key = "api_key=" + settings.FDC_API_KEY
   url = "https://api.nal.usda.gov/fdc/v1/food/" + str(food_id) + '?' + key
   raw_data = requests.get(url)
   data = raw_data.json()
+
+  #Extract Nutritional Data from the nutrient passed in
+  def findNutrientData(inputName): 
+    nutrientData = {}
+    for nutrient in data['foodNutrients']:
+      name = nutrient['nutrient']['name']
+      if (name == inputName):    
+        nutrientData['name'] = name.split(',')[0]
+        nutrientData['amount'] = nutrient['amount']
+        nutrientData['unit'] = nutrient['nutrient']['unitName']
+        break
+      else: 
+        nutrientData['name'] = 'error'
+        nutrientData['amount'] = 'error'
+        nutrientData['unit'] = 'error'
+    return nutrientData
+
+  #Nutrients by Group
+  Macros = ['Energy', 'Total lipid (fat)', 'Protein', 'Carbohydrate, by difference']
+  Fats = ['Fatty acids, total saturated', 'Fatty acids, total trans', 'Cholesterol', 'Fatty acids, total monounsaturated', 'Fatty acids, total polyunsaturated']
+  Carbohydrates = ['Fiber, total dietary', 'Sugars, total including NLEA']
+  AminoAcids = []
+  Vitamins = ['Vitamin C, total ascorbic acid', 'Thiamin', 'Riboflavin', 'Niacin', 'Pantothenic acid', 'Vitamin B-6', 'Folate, total', 'Choline, total', 'Betaine', 'Vitamin B-12', 'Vitamin A, RAE', 'Carotene, beta', 'Carotene, alpha', 'Cryptoxanthin, beta', 'Vitamin A, IU', 'Vitamin E (alpha-tocopherol)', 'Vitamin D (D2 + D3), International Units', 'Vitamin K (phylloquinone)',  ]
+  Minerals = ['Calcium, Ca', 'Iron, Fe', 'Magnesium, Mg', 'Phosphorus, P', 'Potassium, K', 'Sodium, Na', 'Zinc, Zn', 'Copper, Cu', 'Manganese, Mn', 'Selenium, Se']
+
+  #Iterate over Nutrient Groups and extract corresponding data
+  def collectNutrients(Arr): 
+    FoodInfo = []
+    for el in Arr:
+      FoodInfo.append(findNutrientData(el))
+    return FoodInfo
+
+
+
+
+  #Create local variable for viewing
   context = { 
     'Description': data['description'],
-    'Calories': data['foodNutrients'][0]['nutrient']['name']
+    'Macros': collectNutrients(Macros),
+    'Vitamins': collectNutrients(Vitamins),
+    'Minerals': collectNutrients(Minerals),
+    'Carbohydrates': collectNutrients(Carbohydrates),
+    'Fats': collectNutrients(Fats),
+    'AminoAcids': collectNutrients(AminoAcids)
     }
   return render(request, 'search/selection.html', context)
 
